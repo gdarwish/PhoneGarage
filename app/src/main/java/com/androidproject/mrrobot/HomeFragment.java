377533c1e85c6063944040197a4790af718bf866
Ghaith Darwish <ghaith.darwish01@stclairconnect.ca>
2019-10-23 13:24:35 -0400

    Initial Commit

---
+++ app/src/main/java/com/androidproject/mrrobot/HomeFragment.java
@@ -0,0 +1,35 @@
+package com.androidproject.mrrobot;
+
+
+import android.content.res.Resources;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.androidproject.mrrobot.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return view;
+    }
+
+}
