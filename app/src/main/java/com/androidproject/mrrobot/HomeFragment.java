d238f900ee3c53744bd6e2435d0ed7d48a3648d8
web <web@AMSO0055Z-38.local>
2019-10-28 23:19:30 -0400

    #1 Search Bar * Added a search bar to home page

--- app/src/main/java/com/androidproject/mrrobot/HomeFragment.java
+++ app/src/main/java/com/androidproject/mrrobot/HomeFragment.java
@@ -5,12 +5,13 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
-
-import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 
@@ -20,6 +21,13 @@ import java.util.ArrayList;
  */
 public class HomeFragment extends Fragment {
 
+
+    ArrayList<Phone> phones;
+    ArrayList<Phone> searchedPhones;
+
+    EditText searchText;
+    TextView textView;
+    String results = "";
 
     public HomeFragment() {
         // Required empty public constructor
@@ -32,31 +40,58 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        TextView textView = view.findViewById(R.id.textPhone);
-        String text = "";
-
-
-        // Get Device Name of phone with index 100
-        String iPhone = Data.getInstance(getContext()).getPhonesList().get(100).getDeviceName();
-
-        // Accessing phone with multiple simple steps
-        Data data = Data.getInstance(getContext());
-        ArrayList<Phone>  phonesList = data.getPhonesList();
-        Phone firstPhone = phonesList.get(0);
+        searchText = view.findViewById(R.id.searchText);
+        textView = view.findViewById(R.id.textPhone);
 
 
         // Initiate local ArrayList of phones
-        ArrayList<Phone> phones = Data.getInstance(getContext()).getPhonesList();
+        phones = Data.getInstance(getContext()).getPhonesList();
+        displayPhones(phones);
 
-        // Loop through  all phones and display them (or do whatever)
-        for (Phone phone:phones
-             ) {
-            text += phone.getDetailsFormatted() + "\n";
-        }
 
-        textView.setText(text);
+        searchText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                displayPhones(searchPhones(s.toString()));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
 
         return view;
+    }
+
+    private ArrayList<Phone> searchPhones(String searchInput) {
+        searchedPhones = new ArrayList<>();
+        searchInput = searchInput.toUpperCase();
+
+        for(Phone phone: phones) {
+            String device = phone.getDeviceName().toUpperCase();
+            if(device.contains(searchInput)) {
+                searchedPhones.add(phone);
+            }
+        }
+        return searchedPhones;
+    }
+
+    private void displayPhones(ArrayList<Phone> phones) {
+        results = "";
+        // Loop through  all phones and display them (or do whatever)
+        for (Phone phone:phones
+        ) {
+            results += phone.getDetailsFormatted() + "\n";
+        }
+
+        textView.setText(results);
     }
 
 }
